4b788020da4ddc4fa08980e6ab73696fe21ca71a
Kate Fox <katefox100@gmail.com>
2021-03-17 15:47:58 +0000

    Form component displaying correctly

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,27 +3,26 @@ import Winner from "./Winner";
 import Reset from "./Reset";
 import ScoreCardP1 from "./ScoreCard/ScoreCardP1";
 import ScoreCardP2 from "./ScoreCard/ScoreCardP2";
+import Form from "./Form/Form";
 
 const App = () => (
   <>
-      {/* header */}
       <header className="jumbotron mt-4 mb-0">
           <h1>PongPing</h1>
       </header>
 
-      {/* scores */}
       <div className="row mb-4">
             <ScoreCardP1 />
             <ScoreCardP2 />
       </div>
 
-      { /* winner message */}
       <Winner />
 
       <hr />
 
-      { /* reset button */}
       <Reset />
+
+      <Form />
   </>
 );
 
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,93 @@
+import { Component } from "react";
+
+class Form extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            player1Name: "",
+            player2Name: "",
+            winningScore: 21,
+            alternateEvery: 5,
+        };
+
+        this.handleChangeP1Name=this.handleChangeP1Name.bind(this);
+        this.handleChangeP2Name=this.handleChangeP2Name.bind(this);
+        this.handleChangeWS=this.handleChangeWS.bind(this);
+        this.handleChangeAE=this.handleChangeAE.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+    handleChangeP1Name(e) {
+        this.setState({ player1Name: e.currentTarget.value });
+    }
+
+    handleChangeP2Name(e) {
+        this.setState({ player2Name: e.currentTarget.value });
+    }
+
+    handleChangeWS(e) {
+        this.setState({ winningScore: e.currentTarget.value });
+    }
+
+    handleChangeAE(e) {
+        this.setState({ alternateEvery: e.currentTarget.value });
+    }
+
+    handleSubmit(e) {
+        
+    }
+
+    render() {
+        return (
+            <>
+                <div className="form-group">
+                    <label>Player 1 Name</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={ this.player1Name }
+                        onChange={ this.handleChangeP1Name }
+                    />
+
+                    <label>Player 2 Name</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={ this.player2Name }
+                        onChange={ this.handleChangeP2Name }
+                    />
+
+                    <label>Winning Score</label>
+                    <input
+                        className="form-control"
+                        type="number"
+                        value={ this.winningScore }
+                        placeholder="21"
+                        onChange={ this.handleChangeWS }
+                    />
+
+                    <label>Alternate Every</label>
+                    <input
+                        className="form-control"
+                        type="number"
+                        value={ this.alternateEvery }
+                        placeholder="5"
+                        onChange={ this.handleChangeAE }
+                    />
+
+                    <button
+                        className="btn btn-primary"
+                        onClick={ this.handleSubmit }
+                    >Start Game</button>
+                </div>
+                
+            </>
+        )
+    }
+
+
+}
+
+export default Form;
